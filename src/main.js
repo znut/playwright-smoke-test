e92e5fa4081f0d381c74bbdf76f3645b3770c4cf
Nut Sornchumni <snut515@gmail.com>
2020-01-30 01:04:43 +0700

    wait for loading state

--- src/main.js
+++ src/main.js
@@ -10,6 +10,15 @@ import playwright from "playwright";
       }
     });
     const page = await context.newPage('https://new.rurubu.travel/');
+    await page.waitForLoadState({
+      waitUntil: "networkidle2",
+    })
+
+    await page.fill(".SearchBoxTextEditor", "沖縄")
+    await page.click("button.Searchbox__searchButton")
+    await page.waitForLoadState({
+      waitUntil: "networkidle2",
+    })
 
     await page.screenshot({ path: `artifacts/example-${browserType}.png` });
     await browser.close();
